5c8772955fa59e61d09067ad0d7431fd31b1d5fc
akeetuitui <skj6164@naver.com>
2025-02-04 13:26:53 +0900

    <feat>(login): 카카오 로그인 구현 및 안드로이드 패키지 추가

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,8 +1,33 @@
 import 'dart:io';  // 플랫폼 체크를 위해 필요
 import 'package:flutter/material.dart';
-import '../../widgets/login_button.dart';  // 로그인 버튼 불러오기
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
+import '../../widgets/login_button.dart'; 
 
 class LoginPage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance; // Firebase 인증 인스턴스
+
+  Future<void> _kakaoLogin(BuildContext context) async {
+    try {
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+
+      var provider = OAuthProvider('oidc.kakao_login');
+      var credential = provider.credential(
+        idToken: token.idToken,
+        accessToken: token.accessToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+
+      // 로그인 성공 시 다음 화면으로 이동
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("카카오 로그인 실패: $error")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +55,7 @@ class LoginPage extends StatelessWidget {
                 text: "카카오톡으로 시작하기",
                 color: Colors.green,
                 textColor: Colors.black,
-                onPressed: () {
-                  // TODO: 카카오 로그인 로직 추가
-                },
+                onPressed: () => _kakaoLogin(context),
               ),
               // iOS에서만 표시 (애플 로그인)
               if (Platform.isIOS)
