36440c387afc44a310fffec0476086548bbae956
akeetuitui <skj6164@naver.com>
2025-02-04 19:31:21 +0900

    <UI>(login): 로그인화면 UI

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,9 +1,9 @@
-import 'dart:io';  // 플랫폼 체크를 위해 필요
+import 'dart:io'; // 플랫폼 체크를 위해 필요
 import 'package:artlog_app_mvp/ui/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import '../../widgets/login_button.dart'; 
+import '../../widgets/login_button.dart';
 
 class LoginPage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance; // Firebase 인증 인스턴스
@@ -39,50 +39,76 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          // 배경 이미지
           Image.asset(
             'assets/images/splash_screen.png',
             fit: BoxFit.cover,
           ),
           // 로그인 UI
           Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Spacer(),
-              const Text(
-                "나만의 예술취향을\n기록하고 발견하세요",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 30),
-              // 카카오 로그인 버튼 (모든 플랫폼 공통)
-              LoginButton(
-                text: "카카오톡으로 시작하기",
-                color: Colors.yellow,
-                textColor: Colors.black,
-                onPressed: () => _kakaoLogin(context),
-              ),
-              // iOS에서만 표시 (애플 로그인)
-              if (Platform.isIOS)
-                LoginButton(
-                  text: "애플로 시작하기",
-                  color: Colors.black,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    // TODO: 애플 로그인 로직 추가
-                  },
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: const Text(
+                  "나만의 예술취향을\n기록하고 발견하세요",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                    height: 32 / 24,
+                    letterSpacing: -0.005 * 24,
+                    color: Colors.black,
+                  ),
                 ),
-              // Android에서만 표시 (구글 로그인)
-              if (Platform.isAndroid)
-                LoginButton(
-                  text: "구글로 시작하기",
-                  color: Colors.black,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    // TODO: 구글 로그인 로직 추가
-                  },
+              ),
+              const Spacer(), // 버튼과의 간격 조절
+              // 버튼을 하단에 고정
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 40), // 버튼이 너무 아래 붙지 않도록 조정
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min, // 높이 최소화하여 하단 고정
+                    children: [
+                      // 카카오톡 로그인 버튼
+                      LoginButton(
+                        text: "카카오톡으로 시작하기",
+                        color: Colors.yellow,
+                        textColor: Colors.black,
+                        icon: "assets/images/kakao_login.png",
+                        onPressed: () => _kakaoLogin(context),
+                      ),
+                      const SizedBox(height: 12),
+
+                      // 애플 로그인 버튼 (iOS에서만 표시)
+                      if (Platform.isIOS)
+                        LoginButton(
+                          text: "애플로 시작하기",
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          icon: "assets/images/apple_login.png",
+                          onPressed: () {
+                            // TODO: 애플 로그인 로직 추가
+                          },
+                        ),
+                      const SizedBox(height: 12),
+
+                      // 구글 로그인 버튼 (Android에서만 표시)
+                      if (Platform.isAndroid)
+                        LoginButton(
+                          text: "구글로 시작하기",
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          icon: "assets/images/google_login.png",
+                          onPressed: () {
+                            // TODO: 구글 로그인 로직 추가
+                          },
+                        ),
+                    ],
+                  ),
                 ),
-              const SizedBox(height: 50),
+              ),
             ],
           ),
         ],
