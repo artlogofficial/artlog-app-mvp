cb1afcfe2b18cb9993d9667bd42fb5d349cae3fb
akeetuitui <skj6164@naver.com>
2025-02-12 15:57:10 +0900

    <refactor>(containbtn): 버튼 위젯 UI 수정

--- lib/ui/widgets/buttons/contained_button.dart
+++ lib/ui/widgets/buttons/contained_button.dart
@@ -3,31 +3,48 @@ import 'package:flutter/material.dart';
 class ContainedButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-  final Color color;
+  final Color backgroundColor;
   final Color textColor;
 
   const ContainedButton({
     Key? key,
     required this.text,
     required this.onPressed,
-    this.color = Colors.black,
-    this.textColor = Colors.white,
+    this.backgroundColor = const Color(0xFF00E068), 
+    this.textColor = const Color(0xFF222222), 
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: color,
+    return Container(
+      width: 328,
+      height: 52,
+      decoration: ShapeDecoration(
+        color: backgroundColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
         ),
-        padding: EdgeInsets.symmetric(vertical: 16),
       ),
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: TextStyle(color: textColor, fontSize: 16),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 18),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Color(0xFF222222),
+            fontSize: 18,
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w600,
+            height: 1.33,
+            letterSpacing: -0.2,
+          ),
+        ),
       ),
     );
   }
