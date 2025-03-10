b01eacb4554d5c901aa0182520765d858ec4a47b
akeetuitui <skj6164@naver.com>
2025-03-10 22:46:58 +0900

    <docs>(poster): 카드 및 컬렉션 바텀시트 파일 추가

--- lib/ui/pages/profile/profile_page.dart
+++ lib/ui/pages/profile/profile_page.dart
@@ -14,20 +14,65 @@ class ProfilePage extends StatelessWidget {
         showBackButton: false,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const SizedBox(height: 20),
-          Center(
-            child: ProfileAvatar(
-              imageUrl: null, // 기본 프로필 아이콘 사용
-              editMode: true, // 편집 모드 활성화 (연필 아이콘 표시)
-              onEdit: () {
-                print('프로필 편집 클릭됨');
-              },
+          // 프로필 영역
+          Container(
+            width: double.infinity,
+            height: 174,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProfileAvatar(
+                  imageUrl: null, // 기본 프로필 아이콘 사용
+                  editMode: true, // 편집 모드 활성화 (연필 아이콘 표시)
+                  onEdit: () {
+                    print('프로필 편집 클릭됨');
+                  },
+                ),
+                const SizedBox(height: 16),
+                const Description(), // 설명 추가
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class Description extends StatelessWidget {
+  const Description({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '아트로거',
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 4),
+        SizedBox(
+          width: 296,
+          child: Text(
+            '전시 감상 기록으로 나를 발견하고 더 깊이 알아가요. 주1회 기록은 꼭 합니다.',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w400,
+              height: 1.50,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
